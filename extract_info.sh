dd9fe0b84efea5b3fa12bdf8deac15b9eb7208c3
tc-mustang <rjl278@cornell.edu>
2015-09-02 16:32:09 -0400

    Last version

--- extract_info.sh
+++ extract_info.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 a=`pwd | cut -d "/" -f 8`
-echo "Working with $a\\n"
+echo "Working with $a"
+echo ""
 
 b=`awk '{if($13<0.01)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' |wc -l`
 echo "Number of genes q value < 0.01 = $b"
@@ -11,13 +12,13 @@ down=`awk '{if($13<0.01)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' 
 echo "Number of upregulated q value < 0.01 = $up"
 echo "Number of downregulated q value < 0.01 = $down"
 
-awk '{if($13<0.01)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' |wc -l > $a\_$b\_0.01.list
-awk '{if($13<0.01)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' |  awk '{if($10>0)print;}' |wc -l > $a\_$up\_0.01_up.list
-awk '{if($13<0.01)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' |  awk '{if($10<0)print;}' |wc -l > $a\_$down\_0.01_down.list
+awk '{if($13<0.01)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;$1}' > ../Results/$a\_$b\_0.01.list
+awk '{if($13<0.01)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print$1;}' |  awk '{if($10>0)print$1;}'  > ../Results/$a\_$up\_0.01_up.list
+awk '{if($13<0.01)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print$1;}' |  awk '{if($10<0)print$1;}' > ../Results/$a\_$down\_0.01_down.list
 
 
 
-echo "\n"
+echo ""
 
 
 c=`awk '{if($13<0.05)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' |wc -l`
@@ -29,6 +30,6 @@ echo "Number of upregulated q value < 0.05 = $up1"
 echo "Number of downregulated q value < 0.05 = $down1"
 
 
-awk '{if($13<0.05)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' |wc -l > $a\_$c\_0.05.list
-awk '{if($13<0.05)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' |  awk '{if($10>0)print;}' |wc -l > $a\_$up1\_0.05_up.list
-awk '{if($13<0.05)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print;}' |  awk '{if($10<0)print;}' |wc -l > $a\_$down1\_0.05_down.list
+awk '{if($13<0.05)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print$1;}'  > ../Results/$a\_$c\_0.05.list
+awk '{if($13<0.05)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print$1;}' |  awk '{if($10>0)print;}'  > ../Results/$a\_$up1\_0.05_up.list
+awk '{if($13<0.05)print;}' gene_exp.diff | awk '{if($8>1 && $9>1)print$1;}' |  awk '{if($10<0)print;}' > ../Results/$a\_$down1\_0.05_down.list
