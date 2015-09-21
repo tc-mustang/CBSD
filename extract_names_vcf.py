487d82e5410dde65a3abc1b31ec79daa51a403d0
tc-mustang <rjl278@cornell.edu>
2015-09-21 13:41:46 -0400

    Exctract individual from the dosage files. Uses regex to allow for some changes in the IDs.

---
+++ extract_names_vcf.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import itertools
+from operator import itemgetter
+
+dosage = sys.argv[1]
+names = sys.argv[2]
+
+IDs = list()
+columns = list()
+
+columns.append(0)
+
+header = str()
+matches = list()
+
+with open(names) as n:
+        for lines in n:
+                IDs.append(lines)
+        
+with open(dosage) as g:
+        header = g.readline()
+        
+        a = header.split("\t")
+     
+
+print "Los siguientes IDs no se encuentran en el dosage file:"
+
+for elements in IDs:
+        regex = elements.rstrip()+":*\d*"
+        #print regex
+        
+        if re.search(regex,header):
+                out = re.findall(regex, header)
+                matches.append(out)
+        else:
+                
+                print elements.rstrip() 
+
+extract = list(itertools.chain(*matches))
+
+###################################################################### extract the index of every match ##########################################################
+        
+for elements in extract:
+        tmp = a.index(elements)
+        columns.append(tmp)
+        
+
+with open(dosage) as g:
+        for lines in g:
+                a = lines.split("\t")
+                b = itemgetter(*columns)(a)
+                print "\t".join(map(str,b))
+        
+
+
+
+
