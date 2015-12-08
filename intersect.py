e158e36d0cedebbd21dadc4404a963cee777c61a
tc-mustang <rjl278@cornell.edu>
2015-12-08 15:34:18 -0500

    Returns a list of SNPs (Inter, Namikonga, Alberts)

---
+++ intersect.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python 
+
+import sys
+import re
+
+lista = list()
+lista2 = list()
+
+intersect = list()
+Namikonga = list()
+Alberts = list()
+
+with open(sys.argv[1]) as f:
+    for line in f:
+        lista.append(line)
+        
+with open(sys.argv[2]) as f:
+    for line in f:
+        lista2.append(line)
+        if line in lista:
+            intersect.append(line.rstrip())
+        else:
+            Alberts.append(line.rstrip())
+            
+with open(sys.argv[1]) as f:
+    for line in f:
+        if line not in lista2:
+            Namikonga.append(line.rstrip())
+            
+
+f1=open('Intersect.snps', 'w+')
+f1.write("\n".join(intersect))
+f1.close()
+
+f1=open('Namikonga.snps', 'w+')
+f1.write("\n".join(Namikonga))
+f1.close()
+
+f1=open('Alberts.snps', 'w+')
+f1.write("\n".join(Alberts))
+f1.close()
